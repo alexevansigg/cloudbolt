16fca2f8f9ed14adfa583d5538791cb6282280fa
Alex Evans <alexevansigg@hotmail.com>
2018-02-09 12:04:52 +0000

    Removed sample dialog

--- sample_use_of_dialog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from xui.sample_use_of_dialog import views
-
-xui_urlpatterns = [
-    url(r'^add_sample_message/(?P<server_id>\d+)/$', views.add_sample_message,
-        name='add_sample_message'),
-    url(r'^delete_sample_message/(?P<server_id>\d+)/message/(?P<message_id>\d+)/$',
-        views.delete_sample_message, name='delete_sample_message'),
-]
